15c7bf7b9416b564fe3dfdf42f83b5c5890c3500
Максим Щучкин <maxventilator@gmail.com>
2026-04-17 14:11:25 +0300

    sol2

--- task2/solution_02.c
+++ task2/solution_02.c
@@ -14,18 +14,11 @@
 #define MSG_PERM 0666
 #define MAX_NI 1000000000
 
-/* Структура сообщения для IPC */
-typedef struct {
-    long mtype;
-    double sum;
-} msg_t;
+// Структура сообщения для IPC
+typedef struct { long mtype; double sum; } msg_t;
 
-typedef struct {
-    int tid;
-    double a, b;
-    int n;
-    double result;
-} thread_data_t;
+/* Параметры для потока */
+typedef struct { int tid; double a, b; int n; double result; } thread_data_t;
 
 /* Интегрируемая функция */
 double f1(double x) { return 4.0 / (1.0 + x * x); }
@@ -38,7 +31,6 @@ double integrate(double (*f)(double), double a, double b, int n) {
     return h * s;
 }
 
-/* Функция потока */
 void* thread_func(void* arg) {
     thread_data_t* td = (thread_data_t*)arg;
     td->result = integrate(f1, td->a, td->b, td->n);
@@ -46,8 +38,7 @@ void* thread_func(void* arg) {
 }
 
 int main(int argc, char* argv[]) {
-    /* Отключаем буферизацию stderr, чтобы вывод появлялся мгновенно */
-    setbuf(stderr, NULL);
+    setbuf(stderr, NULL); // Отключаем буферизацию stderr
 
     int np = 1, nt = 1;
     if (argc >= 3) { np = atoi(argv[1]); nt = atoi(argv[2]); }
@@ -56,11 +47,14 @@ int main(int argc, char* argv[]) {
 
     double t_start = omp_get_wtime();
 
+    /* Все процессы получат один и тот же msgid */
+    int msgid = msgget(MSG_ID, IPC_CREAT | MSG_PERM);
+    if (msgid < 0) { perror("msgget"); exit(1); }
+
     pid_t spid = 0;
     int mp = 0;
-    int msgid = -1;
 
-    /* Создание тяжелых процессов */
+    /* Ветвление на тяжелые процессы */
     for (int i = 1; i < np; i++) {
         if (spid == 0) {
             mp = i;
@@ -70,7 +64,7 @@ int main(int argc, char* argv[]) {
         }
     }
 
-    /* Распределение работы */
+    /* Распределение области интегрирования */
     double h_proc = 1.0 / np;
     double a_proc = h_proc * mp;
     double b_proc = (mp == np - 1) ? 1.0 : a_proc + h_proc;
@@ -80,18 +74,16 @@ int main(int argc, char* argv[]) {
     pthread_t threads[nt];
     thread_data_t tdata[nt];
 
-    /* Создание потоков */
+    /* 3. Создание потоков */
     for (int t = 0; t < nt; t++) {
         tdata[t].tid = t;
         tdata[t].a = a_proc + h_th * t;
         tdata[t].b = (t == nt - 1) ? b_proc : a_proc + h_th * (t + 1);
         tdata[t].n = n_th;
-        if (pthread_create(&threads[t], NULL, thread_func, &tdata[t]) != 0) {
-            perror("pthread_create"); exit(1);
-        }
+        pthread_create(&threads[t], NULL, thread_func, &tdata[t]);
     }
 
-    /* Ожидание потоков и суммирование */
+    /* Ожидание потоков и локальная сумма */
     double proc_sum = 0.0;
     for (int t = 0; t < nt; t++) {
         pthread_join(threads[t], NULL);
@@ -100,37 +92,25 @@ int main(int argc, char* argv[]) {
                 mp, t, tdata[t].a, tdata[t].b, tdata[t].n, tdata[t].result);
     }
 
-    /*обмен через очередь сообщений System V */
+    /* Суммирование */
     if (mp == 0) {
-        msgid = msgget(MSG_ID, IPC_CREAT | MSG_PERM);
-        if (msgid < 0) { perror("msgget master"); exit(1); }
-
         for (int i = 1; i < np; i++) {
             msg_t msg;
-            if (msgrcv(msgid, &msg, sizeof(double), 0, 0) == -1) {
-                perror("msgrcv master"); exit(1);
-            }
+            msgrcv(msgid, &msg, sizeof(double), 0, 0);
             proc_sum += msg.sum;
         }
-        msgctl(msgid, IPC_RMID, NULL);
     } else {
-        /* Ждём, пока мастер создаст очередь */
-        while ((msgid = msgget(MSG_ID, MSG_PERM)) < 0) {
-            if (errno != ENOENT) { perror("msgget slave"); exit(1); }
-            usleep(100);
-        }
         msg_t msg;
         msg.mtype = mp;
         msg.sum = proc_sum;
-        if (msgsnd(msgid, &msg, sizeof(double), 0) == -1) {
-            perror("msgsnd slave"); exit(1);
-        }
+        msgsnd(msgid, &msg, sizeof(double), 0);
     }
 
     double t_elapsed = omp_get_wtime() - t_start;
 
     if (mp == 0) {
         printf("time=%lf sum= %le\n", t_elapsed, proc_sum);
+        msgctl(msgid, IPC_RMID, NULL);
     }
 
     return 0;
